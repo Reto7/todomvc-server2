7f7a971e2e07fe972d7f979d0144035c61e61822
Reto Kaufmann <reto.kaufmann@adon.li>
2017-05-16 21:14:50 +0200

    .

--- index.js
+++ index.js
@@ -3,20 +3,25 @@
 import express from 'express' // ist es6! kennt node so nicht, muss transpiliert werden !!
 import fs from 'fs'  //filesystem
 import bodyparser from 'body-parser'
+import cors from 'cors'
 
 let app = express()  //express server
-app.use(bodyparser.json())  //fuer express server
+app.use(bodyparser.json())  //fuer express server, achtung () nicht vergessen!
+app.use(cors())  //security  , achtung () nicht vergessen!
+
 
 // GET
 app.get('/items', function (req, res) {
     //res.send('hello world 2')
     //res.send(  [ {id:1} ]  )
     let content = fs.readFileSync('data.json','utf8')
-    res.send(content)
+    res.json(content)
 })
 
 // POST
 app.post('/items', function (req, res) {
+    // data.json muss initial ein leeres array enthalten []
+    // oder natuerlich mit einem objekt drin [{"title":123}]
     console.log(req.body)
     let data = req.body
     // aktuelles file auslesen
@@ -27,7 +32,7 @@ app.post('/items', function (req, res) {
     json.push(data)
     // ganzes file schreiben
     fs.writeFileSync('data.json', JSON.stringify(json), 'utf8')
-    res.send('ok')
+    res.json('ok')
 })
 
 app.listen(3100)
